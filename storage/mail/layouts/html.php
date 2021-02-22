8292b3442be779592e3ed7df84d2fb85d790eed3
Wilmer Arambula <terabytesoftw@gmail.com>
2021-02-22 10:53:41 -0300

    Fixed tests.

--- storage/mail/layouts/html.php
+++ storage/mail/layouts/html.php
@@ -13,29 +13,27 @@ use Yiisoft\Mailer\File;
 ?>
 
 <?php $this->beginPage() ?>
-<!DOCTYPE html PUBLIC "-//W3C//DTD XHTML 1.0 Strict//EN" "http://www.w3.org/TR/xhtml1/DTD/xhtml1-strict.dtd">
-<html xmlns = 'http://www.w3.org/1999/xhtml'>
-    <head>
-        <meta name = 'viewport' content = 'width=device-width'>
-        <meta http-equiv = 'Content-Type' content = 'text/html;charset=UTF-8'>
-    </head>
-    <body class = 'mailer-html-body'>
-        <?php $this->beginBody() ?>
+    <!DOCTYPE html PUBLIC "-//W3C//DTD XHTML 1.0 Strict//EN" "http://www.w3.org/TR/xhtml1/DTD/xhtml1-strict.dtd">
+    <html xmlns = 'http://www.w3.org/1999/xhtml'>
+        <head>
+            <meta name = 'viewport' content = 'width=device-width'>
+            <meta http-equiv = 'Content-Type' content = 'text/html;charset=UTF-8'>
+        </head>
+        <body class = 'mailer-html-body'>
             <?= $content ?>
-        <?php $this->endBody() ?>
 
-        <div class = 'mailer-html-p-content'>
-            <?= Html::img($file->cid()) ?>
-        </div>
-        <div>
-            <?= $signatureTextEmail ?>
-        </div>
-        <div>
-            <br/>
-            <strong>
-                <?= $translator->translate('If you did not make this request you can ignore this email.', [], 'user-mailer') ?>
-            </strong>
-        </div>
-    </body>
-</html>
-<?php $this->endPage();
+            <div class = 'mailer-html-p-content'>
+                <?= Html::img($file->cid()) ?>
+            </div>
+            <div>
+                <?= $signatureTextEmail ?>
+            </div>
+            <div>
+                <br/>
+                <strong>
+                    <?= $translator->translate('If you did not make this request you can ignore this email.', [], 'user-mailer') ?>
+                </strong>
+            </div>
+        </body>
+    </html>
+<?php $this->endPage() ?>
--- storage/mail/layouts/text.php
+++ storage/mail/layouts/text.php
@@ -5,7 +5,5 @@ declare(strict_types=1);
 /** @var string $content */
 
 $this->beginPage();
-    $this->beginBody();
-        echo $content;
-    $this->endBody();
+    echo $content;
 $this->endPage();
